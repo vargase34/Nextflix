641f633171e9a6dafe51d252f76b119c3da18593
Emmanuel Vargas-Zapata <emmanuel.vargas@lc.cuny.edu>
2021-10-28 13:24:02 -0400

    added movie posters into each row using ternary conditions and the api from movie site

--- src/Row.js
+++ src/Row.js
@@ -1,31 +1,41 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios'
-import requests from './requests'
 
-let Row = function ({ title, fetchUrl }){
+const base_url = 'https://image.tmdb.org/t/p/original/'
+
+let Row = function ({ title, fetchUrl }) {
     const [movies, setMovies] = useState([])
 
     // a snippet of code which runs based on a specific condition/variable
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl)
-         
-            console.log(request.data.results)
-            setMovies(request.data.results)
+            setMovies(request.data)
             return request
-        } 
+        }
         fetchData()
-    }, [fetchUrl]) 
-
-    console.log(movies)
+    }, [fetchUrl])
 
     //when row loads, i want to make a request to tdmb to load movies
 
+    // console.clear()
+    console.log(movies)
+
     return (
-        <div>
-            <h2>{ title }</h2>
-            {/* containers inside */}
-            {/* <h1>{ data.results }</h1> */}
+        <div className='row'>
+            <h2>{title}</h2>
+            <div className='row__posters'>
+                {movies.length === 0
+                    ? <p>Loading</p>
+                    : movies.results.map(movie => 
+                        // <p>{movie.title}</p>
+                        <img src={`${base_url}${movie.poster_path}`} alt={movie.title} />
+                    )
+                }
+            </div>
+
+            {/* several row posters */}
+            {/* Check if movies exists */}
         </div>
     )
 }
